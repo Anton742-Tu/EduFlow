28f7522e610b1eb7f11d42075f8832817b45cd05
Anton Tumashov <Tumashovster@Gmail.com>
2025-11-18 22:39:29 +0500

    feat: Complete payments system and user profile enhancements
    - Add Payments model with user, course, and lesson relationships
    - Implement custom management command for payments data population
    - Add payments filtering with ordering, course/lesson and payment method filters
    - Enhance user profile with payments history using nested serializers
    - Add lessons_count field to CourseSerializer using SerializerMethodField
    - Include detailed lessons list in CourseSerializer with LessonSerializer
    - Configure django-filter for advanced payments filtering capabilities
    
    Features:
    - Users can view their payment history in profile
    - Payments can be filtered by course, lesson, payment method
    - Courses show lesson count and detailed lesson information
    - Professional code quality with flake8, black, isort, mypy

--- users/serializers.py
+++ users/serializers.py
@@ -1,48 +1,9 @@
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-
+from django.contrib.auth.password_validation import validate_password
 from .models import User, Payments
 
 
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    """Сериализатор для профиля пользователя"""
-
-    class Meta:
-        model = User
-        fields = ["id", "email", "first_name", "last_name", "phone", "city", "avatar", "date_joined", "last_login"]
-        read_only_fields = ["id", "email", "date_joined", "last_login"]
-
-
-class UserUpdateSerializer(serializers.ModelSerializer):
-    """Сериализатор для обновления профиля пользователя"""
-
-    class Meta:
-        model = User
-        fields = ["first_name", "last_name", "phone", "city", "avatar"]
-
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    """Сериализатор для создания пользователя"""
-
-    password = serializers.CharField(write_only=True, validators=[validate_password])
-    password_confirm = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ["email", "first_name", "last_name", "phone", "city", "avatar", "password", "password_confirm"]
-
-    def validate(self, attrs):
-        if attrs["password"] != attrs["password_confirm"]:
-            raise serializers.ValidationError({"password": "Пароли не совпадают"})
-        return attrs
-
-    def create(self, validated_data):
-        validated_data.pop("password_confirm")
-        user = User.objects.create_user(**validated_data)
-        return user
-
-
+# СНАЧАЛА определяем PaymentsSerializer
 class PaymentsSerializer(serializers.ModelSerializer):
     """Сериализатор для платежей"""
 
@@ -53,3 +14,51 @@ class PaymentsSerializer(serializers.ModelSerializer):
             'paid_lesson', 'amount', 'payment_method'
         ]
         read_only_fields = ['id', 'payment_date']
+
+
+# ПОТОМ определяем User сериализаторы, которые используют PaymentsSerializer
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Сериализатор для профиля пользователя с историей платежей"""
+    payments = PaymentsSerializer(many=True, read_only=True)  # Теперь PaymentsSerializer определен!
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'first_name', 'last_name',
+            'phone', 'city', 'avatar', 'date_joined', 'last_login',
+            'payments'
+        ]
+        read_only_fields = ['id', 'email', 'date_joined', 'last_login']
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    """Сериализатор для обновления профиля пользователя"""
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'phone', 'city', 'avatar'
+        ]
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания пользователя"""
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'email', 'first_name', 'last_name', 'phone',
+            'city', 'avatar', 'password', 'password_confirm'
+        ]
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
--- users/views.py
+++ users/views.py
@@ -15,39 +15,40 @@ from .filters import PaymentsFilter
 class UserViewSet(viewsets.ModelViewSet):
     """
     ViewSet для управления профилями пользователей.
-    Предоставляет полный CRUD для пользователей.
     """
-
-    queryset = User.objects.all()
+    queryset = User.objects.all().prefetch_related('payments')
 
     def get_serializer_class(self):
         """Выбираем сериализатор в зависимости от действия"""
-        if self.action == "create":
+        if self.action == 'create':
             return UserCreateSerializer
-        elif self.action in ["update", "partial_update"]:
+        elif self.action in ['update', 'partial_update']:
             return UserUpdateSerializer
-        return UserProfileSerializer
+        return UserProfileSerializer  # Теперь включает платежи
 
     def get_permissions(self):
         """Разрешаем создание пользователей без авторизации"""
-        if self.action == "create":
+        if self.action == 'create':
             return [AllowAny()]
         return super().get_permissions()
 
-    @action(detail=False, methods=["get"])
+    @action(detail=False, methods=['get'])
     def me(self, request):
         """Эндпоинт для получения текущего пользователя"""
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
-    @action(detail=False, methods=["put", "patch"])
+    @action(detail=False, methods=['put', 'patch'])
     def update_me(self, request):
         """Эндпоинт для обновления текущего пользователя"""
-        serializer = self.get_serializer(request.user, data=request.data, partial=request.method == "PATCH")
+        serializer = self.get_serializer(
+            request.user,
+            data=request.data,
+            partial=request.method == 'PATCH'
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
 
 class PaymentsViewSet(viewsets.ModelViewSet):
     """
