b87bbbb41eba19389034568fb8f758390e45e8fc
Anton Tumashov <Tumashovster@Gmail.com>
2025-11-19 22:49:08 +0500

    fix: Add type annotations to view functions in materials/views.py

--- materials/views.py
+++ materials/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
-from rest_framework import generics, viewsets
+from django.utils.translation import gettext_lazy as _
+from rest_framework import viewsets, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.permissions import AllowAny
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
+from typing import Any
 
 
-def home(request):
+def home(request: Any) -> HttpResponse:
     return HttpResponse(
         "<h1>EduFlow - Образовательная платформа</h1>"
         "<p>Добро пожаловать в EduFlow! Платформа для создания и прохождения курсов.</p>"
@@ -16,24 +18,29 @@ def home(request):
     )
 
 
-@api_view(["GET"])
-def test_api(request):
+@api_view(['GET'])
+def test_api(request: Any) -> Response:
     """Простой тестовый API endpoint"""
-    return Response({"message": "EduFlow API работает!", "status": "success", "version": "1.0"})
+    return Response({
+        'message': 'EduFlow API работает!',
+        'status': 'success',
+        'version': '1.0'
+    })
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     """
     ViewSet для CRUD операций с курсами.
+    Предоставляет все стандартные действия: list, create, retrieve, update, partial_update, destroy
     """
     queryset = Course.objects.all().prefetch_related('lessons')
     serializer_class = CourseSerializer
+
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
     """
     Generic-класс для получения списка уроков и создания нового урока
     """
-
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
@@ -42,7 +49,6 @@ class LessonRetrieveAPIView(generics.RetrieveAPIView):
     """
     Generic-класс для получения одного урока
     """
-
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
@@ -51,7 +57,6 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
     """
     Generic-класс для обновления урока
     """
-
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
@@ -60,6 +65,5 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
     """
     Generic-класс для удаления урока
     """
-
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
